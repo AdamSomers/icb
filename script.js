b919d043b060d0fe5976822b645c916d1d9face4
Adam Somers <adsomers@gmail.com>
2024-08-25 23:09:12 -0700

    uncomment

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function sayHello() {
     // Set up a function to handle the response
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            //document.getElementById("result").innerHTML = xhr.responseText;
+            document.getElementById("result").innerHTML = xhr.responseText;
         }
     };
 
